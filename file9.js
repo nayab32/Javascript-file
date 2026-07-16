48eaf79db677cabd98ab34bfa7ac024c227274ae
Sheraz khan <nayabiqra32@gmail.com>
2026-07-16 22:17:42 +0500

    declare typeof

--- file9.js
+++ file9.js
@@ -1,3 +1,9 @@
-let age = 20;
-let firstname = "iqra";
+// let age = 20;
+// let firstname = "iqra";
+// console.log(typeof age);
+// console.log(typeof(firstname)); "iqra"
+// let mystr = + "32";
+// console.log(typeof mystr);
+let age = "12";
+age = String(age);
 console.log(typeof age);
