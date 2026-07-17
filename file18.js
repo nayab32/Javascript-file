49f9f22b76fb17117826f2465cdbaea98ec41bdc
Sheraz khan <nayabiqra32@gmail.com>
2026-07-17 15:35:33 +0500

    declare else if

---
+++ file18.js
@@ -0,0 +1,12 @@
+let tempIndegree = -10;
+if(tempIndegree < 0){
+    console.log("extremely cold");
+}
+else if(tempIndegree < 20){
+    console.log("normal temprature");
+
+}
+else if (temIndegree < 40){
+    console.log("extremely hot");
+}
+console.log("ice cream");
