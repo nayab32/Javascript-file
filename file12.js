dd08be9e2f3f4ca7eb9ffeecf0b96f0c87c984a0
Sheraz khan <nayabiqra32@gmail.com>
2026-07-17 10:50:25 +0500

    declare undefined bigint

---
+++ file12.js
@@ -0,0 +1,7 @@
+// let firstname ;
+// console.log(typeof firstname);
+// firstname = "iqra";
+// console.log(typeof firstname, firstname);
+let myNumber = BigInt(457687989054322);
+let samemyNumber = 564n;
+console.log(myNumber + samemyNumber);
