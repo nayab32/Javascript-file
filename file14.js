3b669428f49e9fa4d4cfaa79eb27508c7be062b2
Sheraz khan <nayabiqra32@gmail.com>
2026-07-17 12:29:50 +0500

    declare if else condition tuthy fulsy

---
+++ file14.js
@@ -0,0 +1,19 @@
+// let age = 15;
+// if(age>16){
+// console.log("user can play pubg");}
+// else{
+//     console.log("user can play bennybin ");
+// }
+// let num = 14;
+// if(num%2===0){
+// console.log("even");}
+// else{
+//     console.log("odd");
+// }
+let firstname = "";
+if(firstname) {
+    console.log(firstnae);
+}
+else{
+    console.log("firstname is kinda empty");
+}
