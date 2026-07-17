f8b8a798f64daf2977a2129adc1fdae3962f7911
Sheraz khan <nayabiqra32@gmail.com>
2026-07-17 14:31:53 +0500

    declare and or operations

---
+++ file16.js
@@ -0,0 +1,21 @@
+// and or operator
+// let firstNaMe = "iqra";
+// let age = 16;
+
+// if(firstNaMe[0] === "i" && age>18){
+//     console.log("Name start with i and above 18");
+
+// }
+// else{
+//     console.log( "inside else");
+// }
+let firstNaMe = "iqra";
+let age = 16;
+
+
+if(firstNaMe[0] === "i" ||  age>18){
+    console.log("inside if");
+}
+else{
+    console.log( "inside else");
+}
