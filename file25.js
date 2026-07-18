49bc33eb5ce4a003a6af2766ac4e6df32b1a18d2
Sheraz khan <nayabiqra32@gmail.com>
2026-07-18 15:40:06 +0500

    daclare PUSH POP SHIFT UNSHIFT

---
+++ file25.js
@@ -0,0 +1,17 @@
+//  pop and push
+let fruits = ["mango","apple","banana"];
+// fruits.push("banana");
+// console.log(fruits);
+
+// pop
+// let poppedfruit = fruits.pop();
+// console.log(fruits);
+// console.log("popped fruit is" , poppedfruit);
+
+// unshift
+// fruits.unshift("banana");
+// console.log(fruits);
+
+// shift
+fruits.shift("mango");
+console.log(fruits);
