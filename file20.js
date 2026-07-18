0292ca3ed1b5f6bc8cb6b3de0520990007f89eb2
Sheraz khan <nayabiqra32@gmail.com>
2026-07-18 12:39:53 +0500

    declare while loop

---
+++ file20.js
@@ -0,0 +1,15 @@
+let num = 10;
+let total = 0;
+let i = 0;
+total =total + 1;
+total = total + 2;
+total =total+ 3;
+total =total + 4;
+total = total+ 5;
+total =total + 6;
+total =total + 7;
+total =total + 8;
+total =total + 9;
+total =total + 10;
+
+console.log(total);
