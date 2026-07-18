424b6d5c805b29ba7aa0e2d73e6b09eab51b541d
Sheraz khan <nayabiqra32@gmail.com>
2026-07-18 15:12:33 +0500

    declare array

---
+++ file24.js
@@ -0,0 +1,11 @@
+// array
+// let fruits = ["apple","mango","grapes"];
+// let numbers = [2,3,4,5];
+// let mixed = [2,3,4,"mango"];
+// console.log("fruits");
+// console.log(numbers);
+// console.log(mixed);
+
+let fruits = ["apple","mango","grapes"];
+fruits[2] = "banana";
+console.log(fruits);
