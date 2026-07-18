6aa6ff2b1592d42993f0e37fb970c9527a88b8a2
Sheraz khan <nayabiqra32@gmail.com>
2026-07-18 14:13:46 +0500

    declare loop

---
+++ file21.js
@@ -0,0 +1,11 @@
+// for loop
+// for(var i = 1; i<= 6; i++){
+//     console.log(i);
+// }
+// console.log("value of i", i);
+let total = 0;
+let num = 100 ;
+for(i = 1; i<= 100; i++){
+   total=total + i;
+}
+console.log(total);
