9ff70855b1005b5340fbc96e5684d033cd8ae2bd
Sheraz khan <nayabiqra32@gmail.com>
2026-07-19 22:47:21 +0500

    declare clone , slice ,spread, concat

---
+++ file27.js
@@ -0,0 +1,15 @@
+// clone array
+
+let array1 = ["item1","item2"]
+// let array2 = ["item1","item2"]
+// let array2 = [].concat(array1);
+
+// spread operator
+let array2 = [...array1 ,"item3","item4"];
+array1.push("item3");
+// concat array
+// let array2 = array1.slice(0).concat(["item3","item4"]);
+
+console.log(array1===array2);
+console.log(array1)
+console.log(array2)
