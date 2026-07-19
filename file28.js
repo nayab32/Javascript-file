5a78f1f695bd6b431f3e76c6914613dd3e585346
Sheraz khan <nayabiqra32@gmail.com>
2026-07-19 23:29:43 +0500

    declare for loop in array

---
+++ file28.js
@@ -0,0 +1,8 @@
+// for loop in array
+let fruits = ["apple","orange","mango"];
+let fruits2=[];
+for(let i=0; i < fruits.length ; i++){
+
+fruits2.push(fruits[i].toUpperCase());
+}
+console.log(fruits2);
