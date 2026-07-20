aac990191da34157467cee79b26e5b7efc7d7fd6
Sheraz khan <nayabiqra32@gmail.com>
2026-07-20 15:12:57 +0500

    declare object

---
+++ file34.js
@@ -0,0 +1,12 @@
+// objects
+// const person = {name:"iqra", age:21}
+const person = {
+    name:"iqra",
+    age: 21,
+    hobbies: [ "football" , "singing" ],
+
+}
+console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+console.log(person.hobbies);
