11ecce3b853b692014afabaece56677fb6b57b76
Sheraz khan <nayabiqra32@gmail.com>
2026-07-20 14:39:22 +0500

    declare distructing

---
+++ file33.js
@@ -0,0 +1,9 @@
+// array distructuring
+const myarray = ["value1","value2","value3"]
+// let myvar1= myarray[0];
+// let myvar2= myarray[1]; 
+
+let[myvar1,myvar2 ,...myNewarray] = myarray;
+console.log( "value of myvar1", myvar1)
+console.log( "value of myvar2", myvar2)
+console.log(myNewarray);
