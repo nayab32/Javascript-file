a9562a3e9b0e224928130576d71b11bf4529d6fc
Sheraz khan <nayabiqra32@gmail.com>
2026-07-20 15:33:16 +0500

    declare diff between dot and brackets

---
+++ file35.js
@@ -0,0 +1,10 @@
+// different between dot and brackets
+const key = "gmail";
+const person = {
+    name:"iqra",
+    age: 21,
+    hobbies: [ "football" , "singing" ],
+
+}
+person[key]= "iqranayab@gmail.com";
+console.log(person);
