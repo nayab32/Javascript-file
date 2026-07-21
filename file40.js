02171be4c80c19ca4ec52cbc247822967b00571c
Sheraz khan <nayabiqra32@gmail.com>
2026-07-21 15:48:46 +0500

    declare obj inside array

---
+++ file40.js
@@ -0,0 +1,8 @@
+const users = [
+    {userId: 1 , firstName: 'iqra', gender: 'female'},
+    {userId: 2 , firstName: 'ali', gender: 'male'},
+    {userId: 3 , firstName: 'anum', gender: 'female'},
+];
+for(let user of users){
+    console.log(user.userId);
+}
