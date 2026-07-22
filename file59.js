d77f0b46ef16b611a9d9452e1531cfd0a1a8ca9e
Sheraz khan <nayabiqra32@gmail.com>
2026-07-22 22:20:18 +0500

    declare sort methode

---
+++ file59.js
@@ -0,0 +1,11 @@
+// sort methode
+
+let names = ["iqra","sara","ali"];
+names.sort();
+console.log(names);
+
+let number = [1,8,7];
+number.sort(function(a,b){
+    return a-b ;
+});
+console.log(number);
