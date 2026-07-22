5cc0663f284fd913fd2240a58b36420db47cd72a
Sheraz khan <nayabiqra32@gmail.com>
2026-07-22 12:00:35 +0500

    declare arrow function

---
+++ file44.js
@@ -0,0 +1,13 @@
+// function arrow
+const singhappybirthday = () => {
+    console.log("happy birthday to ypu");
+
+}
+singhappybirthday();
+
+ const sumthreenumbers = (number1,number2,number3) =>
+ {
+    return number1+number2+number3;
+ }
+ const ans = sumthreenumbers(2,5,9);
+ console.log(ans);
