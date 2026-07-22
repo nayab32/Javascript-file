f209e652885e76656fd7e8f1e197f6998f8ad68c
Sheraz khan <nayabiqra32@gmail.com>
2026-07-22 21:13:08 +0500

    declare param destructing

---
+++ file52.js
@@ -0,0 +1,11 @@
+// param destructing
+const person = {
+    firstname: "iqra",
+    gender: "female",
+}
+function printdetails(obj){
+    console.log(obj.firstname);
+    console.log(obj.gender);
+
+}
+printdetails(person);
