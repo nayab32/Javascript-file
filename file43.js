59b2ca4daeaf1bafd89e50e7db173f7254bcbed5
Sheraz khan <nayabiqra32@gmail.com>
2026-07-22 11:48:56 +0500

    declare function  expression

---
+++ file43.js
@@ -0,0 +1,11 @@
+// function expression
+//  const singhappybirthday = function(){
+//     console.log("happybirthday");
+//  }
+//  singhappybirthday();
+
+ const sumthreenumbers = function(number1,number2,number3){
+    return number1+number2+number3;
+ }
+ const ans = sumthreenumbers(2,5,9);
+ console.log(ans);
