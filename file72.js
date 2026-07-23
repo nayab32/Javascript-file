c7df4c25495dea64954e0a4dc03181ab8c3532a2
Sheraz khan <nayabiqra32@gmail.com>
2026-07-23 22:01:13 +0500

    declare new keyword

---
+++ file72.js
@@ -0,0 +1,13 @@
+// NEW Keywprd
+function person(name){
+    this.name = name;
+}
+let person1 = new person("iqra")
+console.log(person1.name)
+
+function person(name){
+    this.name = name;
+}
+// newkeyword
+let perso1= new person("iqra");
+console.log(person1.name);
