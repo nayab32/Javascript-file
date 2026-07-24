a2b92625d40e3d44bd3af5df8214ba9acbe4df15
Sheraz khan <nayabiqra32@gmail.com>
2026-07-24 16:21:47 +0500

    declare queryselector

---
+++ script4.js
@@ -0,0 +1,6 @@
+// querry selector
+let firstEle = document.querySelector("p"); //when we needed first elem
+console.dir(firstEle);
+
+let allfirstEl = document.querySelectorAll(".heading"); //all elem
+console.dir(allfirstEl);
