0fcde0fd329f83f25fcbc10b559692fd02348094
Sheraz khan <nayabiqra32@gmail.com>
2026-07-25 11:51:26 +0500

    declare attribute

---
+++ script7.js
@@ -0,0 +1,8 @@
+let div = document.querySelector("div");
+console.log(div);
+
+let id =div.getAttribute("id");
+console.log(id);
+
+let name =div.getAttribute("name");
+console.log(name);
--- sript6.js
+++ sript6.js
@@ -1,2 +1,3 @@
-let h2 = document.querySelector("h2");
+let h2 =
+ document.querySelector("h2");
 console.dir(h2);
