e825a40ba40adf7a2d0b8b5f5f7688c7acc3de32
Sheraz khan <nayabiqra32@gmail.com>
2026-07-25 15:56:59 +0500

    adding new p

--- script11.js
+++ script11.js
@@ -2,3 +2,8 @@ let newbtn = document.createElement("button");
 
 newbtn.innerText="press me";
 console.log(newbtn);
+let div = document.querySelector("div");
+// div.append(newbtn);
+// div.prepend(newbtn);
+// div.before(newbtn);
+div.after(newbtn);
---
+++ script12.js
@@ -0,0 +1,21 @@
+let newheading=document.createElement("h1");
+newheading.innerHTML = "<i>hi, i am new</i>";
+
+
+document.querySelector("body").prepend(newheading);
+
+let para = document.querySelector("p");
+para.remove();
+ newheading.remove();
+
+
+
+
+
+
+
+
+
+
+
+
