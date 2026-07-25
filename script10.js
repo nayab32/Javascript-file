e8757ed17179800f1f990e81e494a3385cb08c7a
Sheraz khan <nayabiqra32@gmail.com>
2026-07-25 14:22:25 +0500

    styleattribute

---
+++ script10.js
@@ -0,0 +1,8 @@
+// style attributes
+let div = document.querySelector("div");
+
+
+div.style.backgroundColor =" green";
+// div.style.visibility = "hidden";
+div.style.fontsize = "30px";
+div.innerText = "hello";
