6d8fe74213e391e270704853173535addb597783
Sheraz khan <nayabiqra32@gmail.com>
2026-07-27 12:54:26 +0500

    declare event

---
+++ script14.js
@@ -0,0 +1,16 @@
+//event objects
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = (evt) => {
+    console.log(evt);
+     console.log(evt.type);
+      console.log(evt.target);
+ console.log(evt.clientX, evt.clientY);
+
+
+
+}
+// let div = document.querySelector("div"); // handling div event
+
+// div.onmouseover = ()=>{
+//     console.log("you are inside div");
+// }
