290fcd4dd7746f86b31e4c63250f5f335800504f
Sheraz khan <nayabiqra32@gmail.com>
2026-07-31 11:04:50 +0500

    declare fetch api

---
+++ script23.js
@@ -0,0 +1,10 @@
+//fetch api
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
