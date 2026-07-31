32e025aa3ca00dbcc3b4c9175823c22208dd73d1
Sheraz khan <nayabiqra32@gmail.com>
2026-07-31 12:47:56 +0500

    declare patch

---
+++ script25.js
@@ -0,0 +1,16 @@
+// patch
+fetch("https://reqres.in/api/users/2",{
+    method: "PATCH",
+    headers:{
+        "content-Type" : "application/json"
+    },
+    body:JSON.stringify({
+        name:"iqra"
+        
+    })
+})
+
+    // })
+.then(response =>response.json())
+.then(data => console.log(data))
+.catch(error =>console.log(error));
